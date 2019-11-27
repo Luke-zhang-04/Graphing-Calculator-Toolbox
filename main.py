8ae1a6c85a7de9cdca0d9b4b5b29fbf051b4be28
Luke <55749227+Luke-zhang-04@users.noreply.github.com>
2019-11-26 21:28:13 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 from GraphingCalculatorToolbox import *
 
-input_screen_size(int(input("Please enter Screen Size: "))) #user inputs screen size
+input_screen_size(int(input("Please enter Screen Size: ")))
 
-draw_grid(int(input("Please enter xMin: ")),int(input("Please enter xMax: ")),int(input("Please enter yMin: ")),int(input("Please enter increment for x: ")),int(input("Please enter increment for y: "))) #User draws up a grid
-
-TOV = makeTableofValues (int(input("Please enter number of points: "))) #Using given data, user enters desired number of points and an equation (in the function)
-plotPoints (TOV[0], TOV[1]) #makeTableofValues returns a tuple of a list of x values and a list of y values
-
-screen.update()
-screen.mainloop()
+draw_axes (int(input("Please enter xMin: ")),int(input("Please enter xMax: ")),int(input("Please enter yMin: ")),int(input("Please enter increment for x: ")),int(input("Please enter increment for y: ")))
+makeTableofValues (int(input("Please enter number of points: ")))
+plotPoints ()
